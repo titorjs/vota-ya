326eed97a0b29ab3f8b59b5fbd0dc0ea7f394d57
titorjs <titoj740@gmail.com>
2024-07-06 22:30:14 -0500

    commit inicial

---
+++ src/main/java/com/vota_ya/vota_ya/VotaYaApplication.java
@@ -0,0 +1,13 @@
+package com.vota_ya.vota_ya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotaYaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotaYaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vota_ya/vota_ya/VotaYaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vota_ya.vota_ya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotaYaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
